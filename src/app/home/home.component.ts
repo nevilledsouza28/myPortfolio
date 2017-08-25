5c8e7c5f5613ed029ada73779c517ab39311fd1e
Neville Dsouza <nevilledsouza28@gmail.com>
2017-08-25 17:19:23 +0530

    modified home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, ViewEncapsulation} from '@angular/core';
 export interface Details {
   name: string,
   designation: string,
+  description: string,
   companyName: string,
   number: string,
   email: string,
@@ -22,9 +23,10 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   details: Details = {
-      name: "neville d'souza",
+      name: "Hi, I'm neville d'souza.",
       companyName: "BNP Paribas India Solutions",
       designation: "Software Engineer (Web Developer)",
+      description: "I have diverse set of skills, ranging from HTML5 + CSS3 + Javascript, all the way to Angular JS 1.x/4, Typescript, Core Java and basics of Oracle SQL.",
       number: "+91-9892761957",
       email: "nevilledsouza28@gmail.com",
       dob: "28th may, 1991",
